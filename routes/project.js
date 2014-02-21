e164d5d8cf42521fb428b1286894209fe1449828
vagrant <vagrant@precise32.(none)>
2014-02-21 07:13:32 +0000

    all your database are belong to us

--- routes/project.js
+++ routes/project.js
@@ -4,9 +4,12 @@ exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
   // query for the specific project and
-  // call the following callback
+  // call the following callback--done
+
+  models.Project.find({"_id" : projectID}).exec(afterQuery);
 
   function afterQuery(err, projects) {
+    console.log("Callback works.");
     if(err) console.log(err);
     res.json(projects[0]);
   }
@@ -18,6 +21,21 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+
+  var project = new models.Project({
+    "title" : form_data["project_title"],
+    "image" : form_data["image_url"],
+    "date" : new Date(form_data["date"]),
+    "summary" : form_data["summary"]
+  });
+
+  project.save(addCallback);
+
+  function addCallback(err) {
+    if(err)
+    console.log(err);
+   res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +43,14 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  models.Project.find({"_id" : projectID}).remove().exec(deleteCallback);
+
+  function deleteCallback(err, projects) {
+  console.log("Delete callback works.");
+   if(err)
+    console.log(err);
+
+  res.send();
+  }
 }
